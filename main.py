f13693f755839c63108170f951ce7a899cb69fe3
VT-Vo <vtvo2006@gmail.com>
2025-09-06 12:46:42 -0700

    5: add circleshape.py and player.py files, edit constants (add PLAYER_RADIUS), edit main.py (add player draw)

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,7 +19,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
